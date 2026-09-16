472c388bcce210931eeb679b08b9256a140e5698
Next Page Technologies Pvt Ltd <56107608+nextpagetech@users.noreply.github.com>
2026-09-16 17:12:13 +0530

    Load Home 3D QA controller

--- includes/footer.php
+++ includes/footer.php
@@ -93,5 +93,6 @@ if ($showSiteShell ?? true) {
     <script src="<?= site_escape(asset_url('js/experience-fixes.js')) ?>" defer></script>
     <script src="<?= site_escape(asset_url('js/support-depth.js')) ?>" defer></script>
     <script src="<?= site_escape(asset_url('js/interactive-pages.js')) ?>" defer></script>
+    <script src="<?= site_escape(asset_url('js/home-3d-qa.js')) ?>" defer></script>
 </body>
 </html>
