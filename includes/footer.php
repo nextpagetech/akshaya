739f15e45c08e89ece14e2c14b095fe925e8cc80
Next Page Technologies Pvt Ltd <56107608+nextpagetech@users.noreply.github.com>
2026-09-17 18:16:18 +0530

    Load home real visual interactions

--- includes/footer.php
+++ includes/footer.php
@@ -98,6 +98,7 @@ if ($showSiteShell ?? true) {
     <script src="<?= site_escape(asset_url('js/home-problem-visuals.js')) ?>" defer></script>
     <script src="<?= site_escape(asset_url('js/home-system-tabs.js')) ?>" defer></script>
     <script src="<?= site_escape(asset_url('js/home-industries-images.js')) ?>" defer></script>
+    <script src="<?= site_escape(asset_url('js/home-real-visuals.js')) ?>" defer></script>
     <script src="<?= site_escape(asset_url('js/inner-page-visual-story.js')) ?>" defer></script>
     <script src="<?= site_escape(asset_url('js/inner-page-meaningful-visuals.js')) ?>" defer></script>
 </body>
