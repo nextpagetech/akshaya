9354807496a9f085bf3828fbc0a8728acb7284ce
Next Page Technologies Pvt Ltd <56107608+nextpagetech@users.noreply.github.com>
2026-09-17 16:53:59 +0530

    Load Home animated problem explorer

--- includes/footer.php
+++ includes/footer.php
@@ -95,6 +95,7 @@ if ($showSiteShell ?? true) {
     <script src="<?= site_escape(asset_url('js/interactive-pages.js')) ?>" defer></script>
     <script src="<?= site_escape(asset_url('js/home-3d-qa.js')) ?>" defer></script>
     <script src="<?= site_escape(asset_url('js/home-semantic-scenes.js')) ?>" defer></script>
+    <script src="<?= site_escape(asset_url('js/home-problem-visuals.js')) ?>" defer></script>
     <script src="<?= site_escape(asset_url('js/inner-page-visual-story.js')) ?>" defer></script>
     <script src="<?= site_escape(asset_url('js/inner-page-meaningful-visuals.js')) ?>" defer></script>
 </body>
