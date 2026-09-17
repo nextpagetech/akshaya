87d016e3844eb3e94e0823851929c1c384833ab3
Next Page Technologies Pvt Ltd <56107608+nextpagetech@users.noreply.github.com>
2026-09-17 17:48:35 +0530

    Load home industries imagery script

--- includes/footer.php
+++ includes/footer.php
@@ -97,6 +97,7 @@ if ($showSiteShell ?? true) {
     <script src="<?= site_escape(asset_url('js/home-semantic-scenes.js')) ?>" defer></script>
     <script src="<?= site_escape(asset_url('js/home-problem-visuals.js')) ?>" defer></script>
     <script src="<?= site_escape(asset_url('js/home-system-tabs.js')) ?>" defer></script>
+    <script src="<?= site_escape(asset_url('js/home-industries-images.js')) ?>" defer></script>
     <script src="<?= site_escape(asset_url('js/inner-page-visual-story.js')) ?>" defer></script>
     <script src="<?= site_escape(asset_url('js/inner-page-meaningful-visuals.js')) ?>" defer></script>
 </body>
